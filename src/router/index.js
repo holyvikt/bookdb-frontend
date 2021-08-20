8747fdfadbcd1d7f2391f20afc0f37a24a5eb82e
holyvikt <holyvikt@fit.cvut.cz>
2021-08-20 21:08:43 +0200

    Init

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Books from '../views/Books.vue'
+import Book from '../views/Book.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Books',
+    component: Books
+  },
+  {
+    path: '/book/:book',
+    name: 'Book',
+    component: Book
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
